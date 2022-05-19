8298294c39b314f6e5fa570fc90baae1db1ddc3b
huscker <egostalker03@mail.ru>
2022-05-19 20:21:35 +0300

    Поправил имя

--- app/queries/users.py
+++ app/queries/users.py
@@ -9,7 +9,7 @@ from app.models import UserStudent
 async def add_user_sql(student: UserStudent) -> None:
     sql = """INSERT INTO users(username, tg_id, name, surname, course , faculty)
              VALUES($1,$2,$3,$4,$5,$6)"""
-    await DB.con.fetchval(sql, student.user_name, student.tg_id, student.name_student,
+    await DB.con.fetchval(sql, student.user_name, student.tg_id, student.name,
                           student.surname, student.course, student.faculty)
 
 
